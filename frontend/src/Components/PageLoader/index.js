df8d499db1d131dbcee169d176f92e5ad3fa0250
choukseaaryan <aaryan2chouksey@gmail.com>
2024-01-29 00:44:56 +0530

    Fixed pageLoader at Login

--- frontend/src/Components/PageLoader/index.js
+++ frontend/src/Components/PageLoader/index.js
@@ -13,6 +13,7 @@ const PageLoader = () => {
 				top: "0",
 				width: "100%",
 				height: "100%",
+				backgroundColor: "rgba(255,255,255,0.6)",
 			}}
 		>
 			<Grid
--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -21,7 +21,7 @@ const Login = () => {
 			return;
 		}
 
-		const emailRegex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
+		const emailRegex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]+$/;
 		if (!emailRegex.test(data.email)) {
 			toast.error("Please enter valid email!");
 			return;
@@ -31,8 +31,8 @@ const Login = () => {
 		dispatch(HANDLE_LOGIN({ payload: data })).then((res) => {
 			if (res?.success) {
 				window.location.href = "/";
-				setLoading(false);
 			}
+			setLoading(false);
 		});
 	};
 
@@ -49,59 +49,56 @@ const Login = () => {
 
 	return (
 		<>
-			{loading ? (
-				<PageLoader />
-			) : (
-				<div className="login">
-					<div className="login__container">
-						<h1>Login</h1>
-						<div className="login__form">
-							<h4>E-mail</h4>
+			{loading && <PageLoader />}
+			<div className="login">
+				<div className="login__container">
+					<h1>Login</h1>
+					<div className="login__form">
+						<h4>E-mail</h4>
+						<input
+							name="email"
+							type="email"
+							placeholder="Enter E-mail"
+							onChange={changeHandler}
+						/>
+
+						<h4>Password</h4>
+						<div className="password__container">
 							<input
-								name="email"
-								type="email"
-								placeholder="Enter E-mail"
+								name="password"
+								type={showPassword ? "text" : "password"}
+								placeholder="Enter Password"
 								onChange={changeHandler}
+								className="password__input"
 							/>
-
-							<h4>Password</h4>
-							<div className="password__container">
-								<input
-									name="password"
-									type={showPassword ? "text" : "password"}
-									placeholder="Enter Password"
-									onChange={changeHandler}
-									className="password__input"
-								/>
-								<button
-									className="toggle__passwordBtn"
-									onClick={toggleShowPassword}
-								>
-									{showPassword ? (
-										<i class="bx bx-hide"></i>
-									) : (
-										<i class="bx bx-show"></i>
-									)}
-								</button>
-							</div>
-
 							<button
-								onClick={submitHandler}
-								className="login__signInButton"
+								className="toggle__passwordBtn"
+								onClick={toggleShowPassword}
 							>
-								Login
+								{showPassword ? (
+									<i class="bx bx-hide"></i>
+								) : (
+									<i class="bx bx-show"></i>
+								)}
 							</button>
 						</div>
 
-						<p className="login__registerPara">
-							Don't have an account?{" "}
-							<a href="/signup" className="login__registerButton">
-								Sign Up
-							</a>
-						</p>
+						<button
+							onClick={submitHandler}
+							className="login__signInButton"
+						>
+							Login
+						</button>
 					</div>
+
+					<p className="login__registerPara">
+						Don't have an account?{" "}
+						<a href="/signup" className="login__registerButton">
+							Sign Up
+						</a>
+					</p>
 				</div>
-			)}
+			</div>
 		</>
 	);
 };
